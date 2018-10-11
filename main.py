7e7681933f7f907673039a42ee0da224cc8260bb
nicksteele <nsteele@duosecurity.com>
2018-10-11 13:50:50 -0400

    git init

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from functools import wraps
+from flask import request, Response
+
+
+app = Flask(__name__)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'eborealis' and password == 'AlmondJoy'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/')
+@requires_auth
+def index():
+    return "<h1>Hello, World!</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, {0}!</h1>'.format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
